43bb33aa1c41f56b4ec6fee4481cda6533392103
Nicholas Lee <nklee2@uci.edu>
2025-01-16 18:13:11 -0800

    UI WIP

--- Assets/Scripts/AbilityButton.cs
+++ Assets/Scripts/AbilityButton.cs
@@ -7,6 +7,13 @@ using UnityEngine.UI;
 public class AbilityBoxHider : MonoBehaviour
 {
     [SerializeField] private bool isButtonActive;
+    [Space]
+    [SerializeField] private float speed;
+    [SerializeField] private Vector3 targetPosition;
+    [SerializeField] private GameObject childObject;
+
+    private bool isSelected;
+    private Vector3 startPosition;
 
     private void Start()
     {
@@ -15,5 +22,49 @@ public class AbilityBoxHider : MonoBehaviour
             this.gameObject.GetComponent<Image>().enabled = false;
             this.gameObject.GetComponent<Button>().enabled = false;
         }
+
+        isSelected = false;
+
+        startPosition = transform.localPosition;
+    }
+
+    public void AbilitySelect()
+    {
+        if (!isSelected)
+        {
+
+
+            if (childObject != null)
+            {
+                childObject.transform.SetParent(null);
+                Debug.Log(childObject.name + " has been removed from its parent.");
+            }
+
+            StartCoroutine(MoveButton());
+            
+            isSelected = true;
+        }
+        else if (isSelected)
+        {
+            //transform.position = Vector3.MoveTowards(transform.position, startPosition, speed * Time.deltaTime);
+            isSelected = false;
+        }
+    }
+
+    private IEnumerator MoveButton()
+    {
+        float currentTimer = Time.deltaTime;
+        float percentDone = 0f;
+
+
+        while (currentTimer < speed)
+        {
+            transform.position = Vector2.Lerp(startPosition, targetPosition, percentDone);
+            percentDone = currentTimer / speed;
+            Debug.Log(percentDone);
+            currentTimer += Time.deltaTime;
+            yield return null;
+        }
+        
     }
 }
