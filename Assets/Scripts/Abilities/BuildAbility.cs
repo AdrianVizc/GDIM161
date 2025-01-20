a7ace3b576866f53db48ee3534535ce98ac09686
ahhhThomas <146419438+ahhhThomas@users.noreply.github.com>
2025-01-20 02:29:49 -0800

    thonking

---
+++ Assets/Scripts/Abilities/BuildAbility.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildAbility : Ability
+{
+    public override void Activate(GameObject parent)
+    {
+        
+    }
+
+    public override void BeginCooldown(GameObject parent)
+    {
+        
+    }
+}
