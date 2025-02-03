f3af03c05fdb205f3ad41319d9a3516c3588be9e
ahhhThomas <146419438+ahhhThomas@users.noreply.github.com>
2025-02-03 12:50:54 -0800

    starting on claymore

---
+++ Assets/Scripts/Abilities/Claymore.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Claymore : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            //blow up do dmg
+            Debug.Log("Detected, destroying");
+            Destroy(this);
+        }
+    }
+}
