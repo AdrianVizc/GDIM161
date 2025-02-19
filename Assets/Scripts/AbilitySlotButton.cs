8cf64ca0c3e629a88fab7b15005e61f8b526da6f
ahhhThomas <146419438+ahhhThomas@users.noreply.github.com>
2025-02-19 12:02:10 -0800

    starting to attempt pairing pain

--- Assets/Scripts/AbilitySlotButton.cs
+++ Assets/Scripts/AbilitySlotButton.cs
@@ -16,4 +16,15 @@ public class AbilitySlotButton : MonoBehaviour
         abilityButtonManager.ClickedAbilitySlot(true);
         abilityButtonManager.SetAbilitySelectPosition(gameObject.transform.position);
     }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("In Slot: " + abilityButtonManager.targetPosition);
+        
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        Debug.Log("Left Slot");
+    }
 }
