8a126a71091b6bf347e232027123a9089d7e9587
ahhhThomas <146419438+ahhhThomas@users.noreply.github.com>
2025-02-19 18:05:51 -0800

    PROGRESS

--- Assets/Scripts/AbilityToPlayer.cs
+++ Assets/Scripts/AbilityToPlayer.cs
@@ -4,6 +4,22 @@ using UnityEngine;
 
 public class AbilityToPlayer : MonoBehaviour
 {
+    private GameObject playerRef;
+    private GameObject wallRef;
+    private GameObject mineRef;
+    private GameObject multishotRef;
+    private GameObject trackerRef;
+    //private GameObject playerRef;
+    private AbilityHolder[] abilityHolderList;
+    private enum Abilities
+    {
+        Wall,
+        Mine,
+        Multishot,
+        Tracker,
+    }
+
+    private Abilities abilityName;
 
     private Ability onel;
     private Ability two2;
@@ -14,16 +30,44 @@ public class AbilityToPlayer : MonoBehaviour
     private GameObject three;
 
     // Start is called before the first frame update
+    private void Start()
+    {
+        playerRef = gameObject.transform.root.gameObject;
+
+        wallRef = GameObject.Find("WallBuilder");
+        wallRef.SetActive(false);
+
+        mineRef = GameObject.Find("MinePlacer");
+        mineRef.SetActive(false);
+
+        multishotRef = GameObject.Find("MultishotHolder");
+        multishotRef.SetActive(false);
+
+        trackerRef = GameObject.Find("Tracker");
+        trackerRef.SetActive(false);
+
+        abilityHolderList = transform.root.GetComponentsInChildren<AbilityHolder>();
+
+        foreach (AbilityHolder ability in abilityHolderList)
+        {
+            //Debug.Log(ability.name);
+            ability.enabled = false;
+        }
+        //Debug.Log(playerRef);
+    }
+
     public void SetAbilities(GameObject player)
     {
-        GameObject play = player;
         Debug.Log("ABILITY 1: " + PlayerPrefs.GetString("1"));
-        //one1 = player.FindWithTag(PlayerPrefs.GetString("1"));
         Debug.Log("ABILITY 2: " + PlayerPrefs.GetString("2"));
         Debug.Log("ABILITY 3: " + PlayerPrefs.GetString("3"));
+        Debug.Log("OWNER: " + player.name);
 
-        three = GameObject.FindWithTag(PlayerPrefs.GetString("3"));
-        Debug.Log("Object: " + three);
+        //string testThree = PlayerPrefs.GetString("3");
+        //Debug.Log(testThree);
+        //three = GameObject.FindWithTag(PlayerPrefs.GetString("3"));
+        //three.SetActive(true);
+        //Debug.Log("Object: " + three.name);
     }
     
 
