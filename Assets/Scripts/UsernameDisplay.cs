95fb21302bfd549c149183c114c9629afa5730de
Dominic Tran <77550542+dominic-tran@users.noreply.github.com>
2025-05-27 17:17:13 -0700

    big push

--- Assets/Scripts/UsernameDisplay.cs
+++ Assets/Scripts/UsernameDisplay.cs
@@ -8,9 +8,11 @@ public class UsernameDisplay : MonoBehaviour
 {
     [SerializeField] private PhotonView playerPV;
     [SerializeField] private TMP_Text text;
+    [SerializeField] private TMP_Text textFacecam;
 
     private void Start()
     {
         text.text = playerPV.Owner.NickName;
+        textFacecam.text = playerPV.Owner.NickName;
     }
 }
