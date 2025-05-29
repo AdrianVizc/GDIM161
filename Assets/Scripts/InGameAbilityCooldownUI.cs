f3f25839391f600b52045b1245bc6b31abe60496
Nicholas Lee <nklee2@uci.edu>
2025-05-29 02:24:58 -0700

    Update
    - Balanced abilities
    - Started on Cooldown UI + script

--- Assets/Scripts/InGameAbilityCooldownUI.cs
+++ Assets/Scripts/InGameAbilityCooldownUI.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -8,6 +9,12 @@ public class InGameAbilityCooldownUI : MonoBehaviour
     [SerializeField] private GameObject abilityIcon1;
     [SerializeField] private GameObject abilityIcon2;
     [SerializeField] private GameObject abilityIcon3;
+    [SerializeField] private GameObject cooldownGray1;
+    [SerializeField] private GameObject cooldownGray2;
+    [SerializeField] private GameObject cooldownGray3;
+    [SerializeField] private TMP_Text timerNumber1;
+    [SerializeField] private TMP_Text timerNumber2;
+    [SerializeField] private TMP_Text timerNumber3;
 
     [Space]
 
@@ -15,7 +22,9 @@ public class InGameAbilityCooldownUI : MonoBehaviour
     [SerializeField] private Sprite dash;
     [SerializeField] private Sprite doubleJump;
     [SerializeField] private Sprite teleport;
-    //[SerializeField] private Sprite ;
+    [SerializeField] private Ability dashAbility;
+    [SerializeField] private Ability doubleJumpAbility;
+    [SerializeField] private Ability teleportAbility;
 
     [Space]
 
@@ -23,7 +32,9 @@ public class InGameAbilityCooldownUI : MonoBehaviour
     [SerializeField] private Sprite multishot;
     [SerializeField] private Sprite mine;
     [SerializeField] private Sprite overload;
-    [SerializeField] private Sprite grenade;
+    [SerializeField] private Ability multishotAbility;
+    [SerializeField] private Ability mineAbility;
+    [SerializeField] private Ability overloadAbility;
 
     [Space]
 
@@ -32,12 +43,27 @@ public class InGameAbilityCooldownUI : MonoBehaviour
     [SerializeField] private Sprite trackingBot;
     [SerializeField] private Sprite shield;
     [SerializeField] private Sprite invisibility;
+    [SerializeField] private Ability wallAbility;
+    [SerializeField] private Ability trackingBotAbility;
+    [SerializeField] private Ability shieldAbility;
+    [SerializeField] private Ability invisibilityAbility;
 
     private void Start()
     {
         SetAbilityIcon(PlayerPrefs.GetString("Slot1"), abilityIcon1);
         SetAbilityIcon(PlayerPrefs.GetString("Slot2"), abilityIcon2);
         SetAbilityIcon(PlayerPrefs.GetString("Slot3"), abilityIcon3);
+
+        cooldownGray1.SetActive(false);
+        cooldownGray2.SetActive(false);
+        cooldownGray3.SetActive(false);
+    }
+
+    private void Update()
+    {
+        CooldownHandler(PlayerPrefs.GetString("Slot1"), cooldownGray1, timerNumber1);
+        CooldownHandler(PlayerPrefs.GetString("Slot2"), cooldownGray2, timerNumber2);
+        CooldownHandler(PlayerPrefs.GetString("Slot3"), cooldownGray3, timerNumber3);
     }
 
     private void SetAbilityIcon(string playerPref, GameObject abilityIconGameObject)
@@ -62,9 +88,9 @@ public class InGameAbilityCooldownUI : MonoBehaviour
             case "Overload":
                 abilityIconGameObject.GetComponent<Image>().sprite = overload;
                 break;
-            case "Grenade":
+            /*case "Grenade":
                 abilityIconGameObject.GetComponent<Image>().sprite = grenade;
-                break;
+                break;*/
             case "Wall":
                 abilityIconGameObject.GetComponent<Image>().sprite = wall;
                 break;
@@ -82,4 +108,55 @@ public class InGameAbilityCooldownUI : MonoBehaviour
                 break;
         }
     }
+
+    private void CooldownHandler(string playerPref, GameObject cooldownObject, TMP_Text timer)
+    {
+        switch (playerPref)
+        {
+            case "Dash":
+                if (dashAbility.cooldownTime != 0)
+                {
+                    cooldownObject.SetActive(true);
+                    timer.text = dashAbility.cooldownTime.ToString();
+                }
+                else
+                {
+                    cooldownObject.SetActive(false);
+                }
+                break;
+            case "Double Jump":
+
+                break;
+            case "Teleport":
+
+                break;
+            case "Multishot":
+
+                break;
+            case "Mine":
+
+                break;
+            case "Overload":
+
+                break;
+            /*case "Grenade":
+
+                break;*/
+            case "Wall":
+
+                break;
+            case "TrackingBot":
+
+                break;
+            case "Shield":
+
+                break;
+            case "Invis":
+
+                break;
+            default:
+
+                break;
+        }
+    }   
 }
